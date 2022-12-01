db4144220bfcf6e84f2d9cf1d83199ea99e7e7c0
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-12-01 18:08:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,12 +19,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6282287219167']
-global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
+global.owner = ['6281386945391']
+global.ownernomer = "6281386945391"
+global.premium = ['6281386945381']
 global.packname = 'Sticker By'
 global.author = 'Sad-Bot'
-global.sessionName = 'papah'  //jangan diganti bro nanti error
+global.sessionName = 'nizar'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
 global.jumlah = '1000000000'
@@ -59,15 +59,15 @@ global.thumb = fs.readFileSync('./media/image/papah.jpg')
 global.faall = fs.readFileSync('./media/image/menu.jpg')
 
 // Url
-global.mygit = 'https://github.com/FahriAdison'
-global.myyt = 'https://bit.ly/Papah-Chan'
+global.mygit = 'https://github.com/nizarrahman'
+global.myyt = 'http://www.nizarrrahman.biz.id'
 global.myytv = ''
 global.mygc = ""
 
 // Engak Ngaruh, Belum Kepasang
 global.botname = 'SAD BOT'
-global.akulaku = 'Bot By Papah'
-global.ytname = 'YT PAPAH' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.akulaku = 'Bot By Nizar'
+global.ytname = 'Nizar' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
